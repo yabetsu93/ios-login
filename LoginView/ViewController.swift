2ffb369db615c4e31620f3f7574b9def528c27d2
yabetsu93 <jabpau93@gmail.com>
2019-09-09 21:04:44 +0900

    Initial Commit

---
+++ LoginView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginView
+//
+//  Created by Jabes Pauya on 2019/09/09.
+//  Copyright © 2019 Jabes Pauya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
